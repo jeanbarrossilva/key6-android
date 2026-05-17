8679d803c207c37bcda910e9f83de9efa0773838
Jean Silva <jeanbarrossilva@outlook.com>
2026-05-16 22:51:41 -0300

    Rename `Keychain.store` to "storage"

--- keychain/src/main/java/com/jeanbarrossilva/key6/keychain/Keychain.kt
+++ keychain/src/main/java/com/jeanbarrossilva/key6/keychain/Keychain.kt
@@ -1,18 +1,18 @@
 /*
  * Copyright © Jean Silva
- * 
+ *
  * This file is part of the Key6 open-source project.
- * 
+ *
  * Key6 is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * Key6 is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see https://www.gnu.org/licenses.
  */
@@ -154,7 +154,7 @@ abstract class Keychain {
    * not yet been removed. The string to which each of them is associated is
    * their identifier, allowing for O(1) retrievals through calls to [get].
    */
-  private val store = HashMap<String, Key>()
+  private val storage = HashMap<String, Key>()
 
   /**
    * Amount of milliseconds required to have passed since the last time in which
@@ -305,7 +305,7 @@ abstract class Keychain {
   ): String {
     val id = Uuid.generateV7().toString()
     val hashedPassword = hash(plainPassword)
-    store[id] = Key(id, title, login, hashedPassword, path)
+    storage[id] = Key(id, title, login, hashedPassword, path)
     return id
   }
 
@@ -353,7 +353,7 @@ abstract class Keychain {
   @Throws(IncorrectMainPasswordException::class)
   suspend operator fun get(keyID: String): Key? {
     unlock()
-    return store[keyID]
+    return storage[keyID]
   }
 
   /**
@@ -364,7 +364,7 @@ abstract class Keychain {
    */
   suspend fun remove(keyID: String) {
     unlock()
-    store.remove(keyID)
+    storage.remove(keyID)
   }
 
   /**
