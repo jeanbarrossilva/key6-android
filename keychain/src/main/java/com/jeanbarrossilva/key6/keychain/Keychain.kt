610a734be756223058693ced901cc3e485cc5cfa
Jean Silva <jeanbarrossilva@outlook.com>
2026-05-27 22:58:54 -0300

    Make `KeychainException.ShortPassword` constructor internal

--- keychain/src/main/java/com/jeanbarrossilva/key6/keychain/Keychain.kt
+++ keychain/src/main/java/com/jeanbarrossilva/key6/keychain/Keychain.kt
@@ -1,18 +1,18 @@
 /*
  * Copyright © Jean Silva
- *
+ * 
  * This file is part of the Key6 open-source project.
- *
+ * 
  * Key6 is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- *
+ * 
  * Key6 is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- *
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see https://www.gnu.org/licenses.
  */
@@ -948,7 +948,7 @@ sealed class KeychainException(message: String) :
    * with less than 8 characters; or in case it is mostly filled with
    * whitespaces.
    */
-  class ShortMainPassword :
+  class ShortMainPassword internal constructor() :
     KeychainException(
       "Main password of a keychain should contain at least 8 characters.")
 }
