05f3fc26f9fc259cf6a81328355935f13682f3cc
Jean Silva <jeanbarrossilva@outlook.com>
2026-07-28 09:26:13 -0300

    Prepend license header to files

--- autofmt/src/root.zig
+++ autofmt/src/root.zig
@@ -1,3 +1,20 @@
+// Copyright © Jean Silva
+//
+// This file is part of the Key6 open-source project.
+//
+// Key6 is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Key6 is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses.
+
 pub const Formatter = @import("core/formatter.zig").Formatter;
 pub const PathFilter = @import("core/path_filter.zig").PathFilter;
 pub const PathsView = @import("core/paths_view.zig").PathsView;
